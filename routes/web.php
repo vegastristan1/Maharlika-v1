51c012a06b46e1872fff670ca9bdb1b3953f9089
Tristan Vegas <vegastristan1@gmail.com>
2024-08-17 22:29:37 +0800

    Created copy of original file for initial

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/original', function () {
+    return view('original file.welcome');
+});
+
+Route::get('/original/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/', function () {
     return view('welcome');

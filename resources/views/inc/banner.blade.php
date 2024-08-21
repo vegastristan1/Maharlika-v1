6a8857a8b78e8c366fe1972932e11191944cd284
Tristan Vegas <vegastristan1@gmail.com>
2024-08-21 18:12:11 +0800

    fix p tag on header description

--- resources/views/inc/banner.blade.php
+++ resources/views/inc/banner.blade.php
@@ -8,8 +8,8 @@
                 <h1>SAMAHAN NG MGA MAHARLIKANG NAGKAKAISA SA PAG-UNLAD INC.</h1>
                 <p> is a non-profit and dynamic organization dedicated to fostering community development and empowering
                     its members. Our mission is to create a supportive and collaborative environment where members
-                    assist each other, driving collective progress towards our shared goals./p>
-                    {{-- <a href="#" class="btn btn-primary">Call to Action</a> --}}
+                    assist each other, driving collective progress towards our shared goals.</p>
+                {{-- <a href="#" class="btn btn-primary">Call to Action</a> --}}
             </div>
         </div>
 

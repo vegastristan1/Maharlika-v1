497056d40a5a8d89576b66500c2cd44e24050027
Tristan <vegastristan1@gmail.com>
2024-08-22 01:32:58 +0800

    adjusted banner text font family

--- resources/views/inc/banner.blade.php
+++ resources/views/inc/banner.blade.php
@@ -5,8 +5,9 @@
     <div class="banner-content d-flex flex-column justify-content-center">
         <div class="container">
             <div class=" banner-content-text">
-                <h1>SAMAHAN NG MGA MAHARLIKANG NAGKAKAISA SA PAG-UNLAD INC.</h1>
-                <p> is a non-profit and dynamic organization dedicated to fostering community development and empowering
+                <h1 class="banner-text">SAMAHAN NG MGA MAHARLIKANG NAGKAKAISA SA PAG-UNLAD INC.</h1>
+                <p class="banner-description"> is a non-profit and dynamic organization dedicated to fostering community
+                    development and empowering
                     its members. Our mission is to create a supportive and collaborative environment where members
                     assist each other, driving collective progress towards our shared goals.</p>
                 {{-- <a href="#" class="btn btn-primary">Call to Action</a> --}}

0162b57ea7144f54370ba8542d8e3d55f44ba312
Tristan <vegastristan1@gmail.com>
2024-08-22 22:11:56 +0800

    fixed contact us info space between

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -14,25 +14,28 @@
             <div class="col-1"></div>
             <div class="col footer-context-details-contact-us">
                 <h4 class="footer-context-details-contact-us-title">Contact Us</h4>
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div class="contact-icon"><i class="fa-solid fa-envelope"></i></div>
-                    <div class="ms-2 me-auto align-self-center ">
-                        <a href="mailto:themaharlikaph@gmail.com"
-                            class="text-color-white text-decoration-none">themaharlikaph@gmail.com</a>
-                    </div>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div class="contact-icon"><i class="fa-solid fa-phone"></i></div>
-                    <div class="ms-2 me-auto align-self-center">
-                        +63-916-905-5879
-                    </div>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div class="contact-icon"><i class="fa-solid fa-location-dot"></i></div>
-                    <div class="ms-2 me-auto align-self-center">
-                        Location Here
-                    </div>
-                </li>
+                <div class="">
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="contact-icon"><i class="fa-solid fa-envelope"></i></div>
+                        <div class="ms-2 me-auto align-self-center ">
+                            <a href="mailto:themaharlikaph@gmail.com"
+                                class="text-color-white text-decoration-none">themaharlikaph@gmail.com</a>
+                        </div>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="contact-icon"><i class="fa-solid fa-phone"></i></div>
+                        <div class="ms-2 me-auto align-self-center">
+                            +63-916-905-5879
+                        </div>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                        <div class="contact-icon"><i class="fa-solid fa-location-dot"></i></div>
+                        <div class="ms-2 me-auto align-self-center">
+                            Location Here
+                        </div>
+                    </li>
+                </div>
+
             </div>
         </div>
         <hr>

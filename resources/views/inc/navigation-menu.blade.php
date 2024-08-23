9549d04a6379fc48fe9ae3b5eea3e79fb9053802
Tristan <vegastristan1@gmail.com>
2024-08-23 20:54:12 +0800

    removed hamburger menu that has not enough item

--- resources/views/inc/navigation-menu.blade.php
+++ resources/views/inc/navigation-menu.blade.php
@@ -4,8 +4,14 @@
             <img class="c-brand-logo" src="{{ asset('images/maharlika.png') }}"
                 alt="Samahan ng mga Maharlikang nagkakaisa sa pag-unlad Inc.">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+        <ul class="navbar-nav justify-content-end align-items-end flex-grow-1 pe-3">
+            <li class="nav-item d-flex justify-content-center">
+                <a class="nav-link c-nav-menu-item" aria-current="page" href="#about">About Us</a>
+            </li>
+        </ul>
+        {{-- <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
             aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars fa-xl"></i>
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
@@ -21,9 +27,9 @@
                     <li class="nav-item d-flex justify-content-center">
                         <a class="nav-link c-nav-menu-item" aria-current="page" href="#about">About Us</a>
                     </li>
-                    {{-- <div class="btn btn-c-nav-menu c-nav-menu-item">Login</div> --}}
+                    <div class="btn btn-c-nav-menu c-nav-menu-item">Login</div>
                 </ul>
             </div>
-        </div>
+        </div> --}}
     </div>
 </nav>

b1f814658407fcccdbcd1a7d39b557d2b21e634d
Tristan Vegas <vegastristan1@gmail.com>
2024-08-25 23:30:07 +0800

    render config docker with nginx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,10 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url)
     {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('http');
         }
     }
 }

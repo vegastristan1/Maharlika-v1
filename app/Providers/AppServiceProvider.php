305a6baef63892142541df8508f19e9002f0568b
Tristan Vegas <vegastristan1@gmail.com>
2024-08-26 11:34:30 +0800

    ssl https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if (env('APP_ENV') == 'production') {
-            $url->forceScheme('http');
+            $url->forceScheme('https');
         }
     }
 }
